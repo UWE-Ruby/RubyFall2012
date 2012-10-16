9d3cd47cf0772e1c2056e9f0d9960116155581cf
Nicole Lewis <lewis.nicole@gmail.com>
2012-10-16 07:02:20 -0700

    lolo's week1 rspec

--- week1/homework/strings_and_rspec_spec.rb
+++ week1/homework/strings_and_rspec_spec.rb
@@ -12,15 +12,17 @@ describe String do
 		before(:all) do
 			@my_string = "Renée is a fun teacher. Ruby is a really cool programming language"
 		end
-		it "should be able to count the charaters"
+		it "should be able to count the charaters" do
+			result = @my_string.length 
+			result.should eq(66)
+		end
 		it "should be able to split on the . charater" do
-			pending
-			result = #do something with @my_string here
+			result = @my_string.split(/\./)
 			result.should have(2).items	
 		end
 		it "should be able to give the encoding of the string" do
-			pending
-			#should eq (Encoding.find("UTF-8"))
+			result = @my_string.encoding
+			result.should eq (Encoding.find("UTF-8"))
 		end
 	end
 end
