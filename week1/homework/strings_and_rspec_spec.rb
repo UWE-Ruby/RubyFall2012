25630cd17ee08a8e41b2e53622430790e5ae3582
Peter E. Granger <peter@granger.net>
2012-10-15 23:21:14 -0700

    Playing around with the string/rspec exercise

--- week1/homework/strings_and_rspec_spec.rb
+++ week1/homework/strings_and_rspec_spec.rb
@@ -11,16 +11,41 @@ describe String do
 	context "When a string is defined" do
 		before(:all) do
 			@my_string = "Renée is a fun teacher. Ruby is a really cool programming language"
+            puts "Starting string: \"#@my_string\""
 		end
-		it "should be able to count the charaters"
-		it "should be able to split on the . charater" do
-			pending
-			result = #do something with @my_string here
-			result.should have(2).items	
+
+        after(:all) do
+            puts "\nFooling around some more with strings. Splitting on 'a'"
+            counter = 0
+			result = @my_string.split(/a/)
+            result.each do |current_item|
+                puts "#{counter}: \"#{current_item}\""
+                counter += 1
+            end
+
+            rebuilt = ""
+            result.each do |current_item|
+                counter -= 1
+                rebuilt += current_item
+                if (counter > 0) then rebuilt += "a" end
+            end
+            puts "Rebuilt string = \"#{rebuilt}\""
+
+            puts "This is#{if (rebuilt != @my_string) then " not" else "" end} equal to the original."
+        end
+
+		it "should be able to count the characters" do
+            @my_string.should have(66).characters
+            @my_string.length.should eq 66
+        end
+
+		it "should be able to split on the . character" do
+			result = @my_string.split(/\.\s*/)
+			result.should have(2).items
 		end
+
 		it "should be able to give the encoding of the string" do
-			pending
-			#should eq (Encoding.find("UTF-8"))
+			@my_string.encoding.should eq (Encoding.find("UTF-8"))
 		end
 	end
 end
