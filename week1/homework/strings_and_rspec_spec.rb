2cdf62023762634fc088362d93033ed514d0727a
phamdt <pham.danny.t@gmail.com>
2012-10-16 15:40:13 -0700

    Update week1/homework/strings_and_rspec_spec.rb

--- week1/homework/strings_and_rspec_spec.rb
+++ week1/homework/strings_and_rspec_spec.rb
@@ -5,14 +5,16 @@ describe String do
 			@my_string = "Renée is a fun teacher. Ruby is a really cool programming language"
 		end
 		it "should be able to count the charaters"
+                        count = @my_string.length
 		it "should be able to split on the . charater" do
 			pending
-			result = #do something with @my_string here
+			result = @my_string.split("\.")
 			result.should have(2).items	
 		end
 		it "should be able to give the encoding of the string" do
 			pending
 			#should eq (Encoding.find("UTF-8"))
+                        encoding = @my_string.encoding
 		end
 	end
 end
