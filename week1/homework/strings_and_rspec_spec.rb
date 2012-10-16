5552f604ee5190624cbcc4e53043b4065e7e4034
Kelli A. Mohr <kellimohr@gmail.com>
2012-10-15 17:54:17 -0700

    Homework changes

--- week1/homework/strings_and_rspec_spec.rb
+++ week1/homework/strings_and_rspec_spec.rb
@@ -1,18 +1,20 @@
-# encoding: utf-8
+encoding: utf-8
 describe String do
 	context "When a string is defined" do
 		before(:all) do
 			@my_string = "Renée is a fun teacher. Ruby is a really cool programming language"
 		end
 		it "should be able to count the charaters"
+			@my_string.should have(5).characters
+
 		it "should be able to split on the . charater" do
 			pending
 			result = #do something with @my_string here
 			result.should have(2).items	
 		end
 		it "should be able to give the encoding of the string" do
-			pending
-			#should eq (Encoding.find("UTF-8"))
+			
+			should eq (Encoding.find("UTF-8"))
 		end
 	end
 end
