46fec62d4e07c3ab69e0f7f25ca9e2aa9a116a71
Cheri <cherimarie@gmail.com>
2012-10-16 22:58:33 -0700

    did a poor job of trying to make tests pass

--- week1/homework/strings_and_rspec_spec.rb
+++ week1/homework/strings_and_rspec_spec.rb
@@ -9,17 +9,24 @@
 
 describe String do
 	context "When a string is defined" do
+
 		before(:all) do
 			@my_string = "Renée is a fun teacher. Ruby is a really cool programming language"
 		end
-		it "should be able to count the charaters"
+
+		it "should be able to count the charaters"do
+			@my_string.should have(66).characters
+		end
+
 		it "should be able to split on the . charater" do
-			pending
-			result = #do something with @my_string here
+			
+			result = @my_string.split(/\s*\.\s*/)   #do something with @my_string here
 			result.should have(2).items	
 		end
+
 		it "should be able to give the encoding of the string" do
-			pending
+			
+			@my_string.encoding should eq utf-8
 			#should eq (Encoding.find("UTF-8"))
 		end
 	end
