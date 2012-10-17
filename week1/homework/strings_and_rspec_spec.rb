0abf545854ea051262a3f35bad8f7263aaceee8e
Nicholas Palaniuk <npalaniuk@gmail.com>
2012-10-16 18:35:43 -0700

    Fix errors

--- week1/homework/strings_and_rspec_spec.rb
+++ week1/homework/strings_and_rspec_spec.rb
@@ -13,26 +13,23 @@ describe String do
 		before(:all) do
 			@my_string = "Renée is a fun teacher. Ruby is a really cool programming language"
 		end
-<<<<<<< HEAD
 		it "should be able to count the characters" do
 			@my_string.length.should eq 66
 		end
 		it "should be able to split on the . character" do
 			
-=======
+
 		it "should be able to count the charaters" do
 			@my_string.should have(@my_string.size).characters
 		end
 		it "should be able to split on the . charater" do
->>>>>>> ea9aa93fcf3644ee7d901c086bc1b1ebc77f6f44
+
 			result = @my_string.split('.')
 			result.should have(2).items	
 		end
 		it "should be able to give the encoding of the string" do
-<<<<<<< HEAD
 
-=======
->>>>>>> ea9aa93fcf3644ee7d901c086bc1b1ebc77f6f44
+
 			@my_string.encoding.should eq (Encoding.find("UTF-8"))
 		end
 	end
