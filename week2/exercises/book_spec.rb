a9008c2f59fa7c89cddc2f5395ff83f16cd6c491
YanLi0307 <leeyeon0307@gmail.com>
2012-10-16 23:40:28 -0700

    removing changes to book_spec

--- week2/exercises/book_spec.rb
+++ week2/exercises/book_spec.rb
@@ -3,16 +3,12 @@ describe Book do
 	before :each do
 		@book = Book.new("Harry Potter", 200)
 	end
-
 	before :each do
 		@book = Book.new("Harry Potter", 200)
-    @page_count = Book.new("Harry Potter", 200)
 	end
-
 	it "should respond to title" do
 		@book.should respond_to "title"
 	end
-
 	it "should return the page count" do
 		@book.page_count.should eq "Page count is 200"
 	end
