1651d34f79aa0f501bb23f19c97a71d9ca5c1ead
Ryan Calderon <rcalderon04@gmail.com>
2012-10-16 18:23:07 -0700

    added my homework wk1

--- week1/homework/strings_and_rspec_spec.rb
+++ week1/homework/strings_and_rspec_spec.rb
@@ -1,5 +1,9 @@
 # encoding: utf-8
 
+# Ryan Calderon
+# 10/16/12
+# Week 1 homework
+
 # Please make these examples all pass
 # You will need to change the 3 pending tests
 # You will need to write a passing test for the first example 
@@ -12,14 +16,19 @@ describe String do
 		before(:all) do
 			@my_string = "Renée is a fun teacher. Ruby is a really cool programming language"
 		end
-		it "should be able to count the charaters"
+		it "should be able to count the charaters" do
+		  @my_string.should have(67).characters
+	  end
 		it "should be able to split on the . charater" do
-			pending
-			result = #do something with @my_string here
+			result = @my_string.split(/\s*\.\s*/)
+			#do something with @my_string here
 			result.should have(2).items	
 		end
 		it "should be able to give the encoding of the string" do
-			pending
+		  #.encoding is not working for me as I have Ruby 1.8.7
+			stringEncoding = @my_string.encoding
+			stringEncoding.should eq("UTF-8")
+			#stringEncoding.should eq (Encoding.find("UTF-8"))
 			#should eq (Encoding.find("UTF-8"))
 		end
 	end
