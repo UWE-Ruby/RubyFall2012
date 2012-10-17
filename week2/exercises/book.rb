409413d8918b0a82a53caaadd3e2bc5376b05903
wsugg <sugg.will@gmail.com>
2012-10-17 14:33:01 -0700

    book_spec passes

--- week2/exercises/book.rb
+++ week2/exercises/book.rb
@@ -1,2 +1,18 @@
+#!/usr/bin/env 
+
 class Book
+	attr_accessor :title, :count
+
+	def initialize(title="title" ,count)
+		@title = title
+		@page_count = count
+	end
+
+	def title()
+		@title
+	end
+
+	def page_count()
+		@page_count
+	end
 end
--- week2/exercises/book_spec.rb
+++ week2/exercises/book_spec.rb
@@ -1,13 +1,15 @@
+#!/usr/bin/env  
+
 require './book.rb'
 describe Book do
 	before :each do
 		@book = Book.new("Harry Potter", 200)
 	end
 	it "should respond to title" do
-		@book.should respond_to "title"
+		@book.should respond_to :title
 	end
 
 	it "should return the page count" do
-		@book.page_count.should eq "Page count is 200"
+		"Page count is #{@book.page_count}".should eq "Page count is 200"
 	end
 end
