d6d55214dc923ca2761d8212e6b1bd83e97ce71b
Nicholas Palaniuk <npalaniuk@gmail.com>
2012-10-17 11:12:32 -0700

    Finish book spec

--- week2/exercises/book.rb
+++ week2/exercises/book.rb
@@ -1,2 +1,22 @@
 class Book
+	
+	attr_accessor :title, :page_count
+
+	def initialize(title, page_count)
+		@title = title
+		@page_count = page_count
+	end
+
+	def page_count
+		return "Page count is #{@page_count}"
+	end
+
 end
+
+=begin
+book1 = Book.new("Harry Potter")
+puts book1.title
+
+book1.title = "Serpent"
+puts book1.title
+=end
