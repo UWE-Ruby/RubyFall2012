ae0ff6a0aa6813311c6a27fd88e742f3699c7d50
Steven Curtiss <Steven.Curtiss@infospace.com>
2012-10-19 14:59:13 -0700

    book work

--- week2/exercises/book.rb
+++ week2/exercises/book.rb
@@ -1,2 +1,10 @@
 class Book
+  attr_accessor :title
+  def initialize(title, page_count)
+    @title = title
+    @page_count = page_count
+  end
+  def page_count
+    "Page count is " + @page_count.to_s
+  end
 end
