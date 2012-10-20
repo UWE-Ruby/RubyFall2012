ed8ebae3eeb050c8bd364f358cc5c7dbb2be75df
Kris Luminar <kris.luminar@gmail.com>
2012-10-20 11:24:04 -0700

    did some exercises.

--- week2/exercises/book.rb
+++ week2/exercises/book.rb
@@ -1,2 +1,12 @@
 class Book
+  attr_reader :title
+
+  def initialize title, page_count
+    @title = title
+    @page_count = page_count
+  end
+
+  def page_count
+    %Q[Page count is #@page_count]
+  end
 end
