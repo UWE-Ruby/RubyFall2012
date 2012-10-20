0b0db58e4e613275687fe9bb13f78c57039edd5f
Sol W <capnsol@gmail.com>
2012-10-20 00:09:49 -0700

    commiting book.rb before updating repo with other HW

--- week2/exercises/book.rb
+++ week2/exercises/book.rb
@@ -1,2 +1,25 @@
 class Book
+
+  attr_accessor :title
+  
+  def initialize(title, page_count)
+    @title = title
+    @page_count = page_count
+  end
+
+  def title
+    @title
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def title=(title)
+    @title
+  end
+
+  def page_count=(page_count)
+    @page_count
+  end
 end
