3626f5e227af102622dc10bd43a574fcb44083e2
gregstallingsuw <gregstallingsuw@gmail.com>
2012-10-20 22:07:06 -0700

    Update coding portion of week2 homework

--- week2/homework/simon_says.rb
+++ week2/homework/simon_says.rb
@@ -1,31 +1,31 @@
 module SimonSays
 
-  # Echos back the given string.
+  # Returns the given str.
   def echo(str)
     str
   end
 
-  # Returns the given string in uppercase.
+  # Returns a copy of str with all lowercase letters replaced with their uppercase counterparts.
   def shout(str)
     str.upcase
   end
 
-  # Repeats string a given number of times.
+  # Returns a copy of str, repeated count times and separated with a single space.
   def repeat(str, count = 2)
     raise ArgumentError, "count must be a positive integer" if !(count.is_a? Integer) || count < 1
 
     (1..count).collect{ str }.join(' ')
   end
 
-  # Returns the portion of a word from the beginning of the word to the specified position.
-  def start_of_word(word, position)
-    raise ArgumentError, "position must be an integer" if !position.is_a? Integer
-    raise ArgumentError, "no character at position #{position} in word" unless position.between?(1, word.length)
+  # Returns the portion of word from the beginning of word to the given length.
+  def start_of_word(word, length)
+    raise ArgumentError, "length must be an integer greater or equal to 0" if !(length.is_a? Integer) || length < 0
+    raise ArgumentError, "length is greater than the length of word" if length > word.length
 
-    word[0..(position - 1)]
+    word[0, length]
   end
 
-  # Returns the first word in a string.
+  # Returns a new string of the first word in str.
   def first_word(str)
     str.split(' ')[0]
   end
