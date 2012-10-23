ac6fbc3be7702f5d6439b3bc94842a1076ed0a52
Nicole Lewis <lewis.nicole@gmail.com>
2012-10-22 21:57:32 -0700

    Week 2 exercises and homework code

---
+++ week2/homework/simon_says.rb
@@ -0,0 +1,23 @@
+module SimonSays
+	def echo(what)
+		what
+	end
+	
+	def shout(what)
+		what.upcase
+	end
+	
+	def repeat(what,number=2)
+		((what + " ")*number).strip
+	end
+	
+	def start_of_word(the_word, characters_to_keep)
+		the_word.slice(0..characters_to_keep-1)
+		# passing a range, but a) it starts at 0 not 1, b) using the parm 
+	end
+	
+	def first_word(what)
+		what.split[0]
+		# split splits string by whitespace and results in an array :)
+	end
+end 
