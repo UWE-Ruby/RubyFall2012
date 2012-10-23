cb2954db702e2dcb3b7d5c1056cae7048e46a044
Chris Barcroft <ChrisABarcroft@gmail.com>
2012-10-23 12:00:44 -0700

    Committing inclass work from last week

--- week2/exercises/book.rb
+++ week2/exercises/book.rb
@@ -1,2 +1,12 @@
 class Book
+  attr_accessor :title, :page_count
+  
+  def initialize(title, page_count)
+    @title = title
+    @page_count = page_count
+  end
+  
+  def page_count
+    "Page count is #{@page_count}"
+  end
 end
--- week2/exercises/mad_libs.rb
+++ week2/exercises/mad_libs.rb
@@ -4,5 +4,12 @@ puts "Please enter an adjective"
 adjective = gets.chomp
 puts "Please enter a past tense action verb"
 verb_past_tense = gets.chomp
-story = "The #{adjective} #{noun} #{verb_past_tense} past the graveyard"
+puts "Please enter a second noun"
+noun2 = gets.chomp
+puts "Please enter another adjective"
+adjective2 = gets.chomp
+puts "Please enter an exclamation"
+exclamation = gets.chomp.upcase
+
+story = "The #{adjective} #{noun} #{verb_past_tense} past the graveyard. The #{noun} spotted a #{adjective2} #{noun2}, and yelled '#{exclamation}!'"
 puts story
