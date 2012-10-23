66857d2ce6f50893b13251824d03b76b72e9017d
Price Hardman <PriceHardman@gmail.com>
2012-10-23 15:38:25 -0700

    answered homework questions

--- week2/homework/simon_says.rb
+++ week2/homework/simon_says.rb
@@ -8,14 +8,15 @@ module SimonSays
 	end
 
 	def first_word(st)
-		st.split.first
+		st.split(' ')[0]
 	end
 
 	def start_of_word(st,i)
-		st[0...i]
+		st.slice!(0..i-1)
 	end
 	
 	def repeat(st, t=2)
-		([st]*t).join(' ')
+		(st+" ")*(t-1)+st  # or could use Array join method.
+		
 	end
 end
