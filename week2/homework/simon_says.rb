fd3ea6aa9f5cc38edae2decc4e90b7f31b8c5f73
Steven Curtiss <Steven.Curtiss@infospace.com>
2012-10-22 23:32:28 -0700

    Week 2 Homework

---
+++ week2/homework/simon_says.rb
@@ -0,0 +1,21 @@
+module SimonSays
+  def echo(textToEcho)
+    textToEcho
+  end
+
+  def shout(textToShout)
+    textToShout.upcase
+  end
+
+  def repeat(textToRepeat, count=2)
+    Array.new(count, textToRepeat + " ").inject(:+).chomp(" ")
+  end
+
+  def start_of_word(word, count)
+    word[0...count]
+  end
+
+  def first_word(sentence)
+    sentence.scan(/[\w']+/)[0]
+  end
+end
