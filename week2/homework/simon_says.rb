b552c4bc159377d4985d2ba326a6df650edb806e
Brian Ward <brianmatthewward@gmail.com>
2012-10-23 18:29:42 -0700

    moved comment block back to left side

--- week2/homework/simon_says.rb
+++ week2/homework/simon_says.rb
@@ -26,27 +26,27 @@ module SimonSays
     word = phrase.split(' ')[0]
   end
 
-  =begin
-  	def echo(st)
-  		st
-  	end
-  	
-  	def shout(st)
-  		st.upcase
-  	end
+=begin
+	def echo(st)
+		st
+	end
+	
+	def shout(st)
+		st.upcase
+	end
 
-  	def first_word(st)
-  		st.split.first
-  	end
+	def first_word(st)
+		st.split.first
+	end
 
-  	def start_of_word(st,i)
-  		st[0...i]
-  	end
-  	
-  	def repeat(st, t=2)
-  		([st]*t).join(' ')
-  	end
-  =end
+	def start_of_word(st,i)
+		st[0...i]
+	end
+	
+	def repeat(st, t=2)
+		([st]*t).join(' ')
+	end
+=end
   
 end
 
