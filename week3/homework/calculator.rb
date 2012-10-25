56d34203dc5c46ea9308543be1b7c905e6621dc7
Ben Woodall <mail@benwoodall.com>
2012-10-25 07:49:30 -0700

    added specs and made calculator class

--- week3/homework/calculator.rb
+++ week3/homework/calculator.rb
@@ -1,2 +1,20 @@
 class Calculator
+
+  def sum(*num)
+    num.map {|n| n.inject(0,:+)}.first
+  end
+
+  def multiply(num1, num2=1)
+    return num1 * num2 if num1.class == Fixnum
+    num1.inject(:*)
+  end
+
+  def power(num1, num2)
+    num1 ** num2
+  end
+
+  def factorial(num)
+    (1..num).inject(1,:*)
+  end
+
 end
