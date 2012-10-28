ca9d4ada7609a59ffe15261e554ab37abf46fb9d
Strand McCutchen <strand@bettermistak.es>
2012-10-28 14:09:02 -0700

    Homework for week 3

--- week3/homework/calculator_spec.rb
+++ week3/homework/calculator_spec.rb
@@ -24,22 +24,43 @@ describe Calculator do
     end
   end
   
-  # Once the above tests pass, 
-  # write tests and code for the following:
-  
-  it "multiplies two numbers"
+  describe "#multiply" do
+    it "multiplies two numbers" do
+      @calculator.multiply(5, 9).should == 45
+    end
 
-  it "multiplies an array of numbers"
-  
-  it "raises one number to the power of another number"
+    it "multiplies an array of numbers" do
+      @calculator.multiply_array([1, 3, 5, 7, 9]).should == 945
+    end
+  end
+    
+  describe "#exponent" do
+    it "raises one number to the power of another number" do
+      @calculator.exponent(3,3).should == 27
+    end
+  end
   
   # http://en.wikipedia.org/wiki/Factorial
   describe "#factorial" do
-    it "computes the factorial of 0"
-    it "computes the factorial of 1"
-    it "computes the factorial of 2"
-    it "computes the factorial of 5"
-    it "computes the factorial of 10"
+    it "computes the factorial of 0" do
+      @calculator.factorial(0).should == 1
+    end
+
+    it "computes the factorial of 1" do
+      @calculator.factorial(1).should == 1
+    end
+
+    it "computes the factorial of 2" do
+      @calculator.factorial(2).should == 2
+    end
+
+    it "computes the factorial of 5" do
+      @calculator.factorial(5).should == 120
+    end
+
+    it "computes the factorial of 10" do
+      @calculator.factorial(10).should == 3628800
+    end
   end
 
 end
