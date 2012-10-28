a855273763a603e0afe468e7472c2583f8afa1ac
Nikky Southerland <nikky@uw.edu>
2012-10-28 11:07:23 -0700

    doop

--- week2/homework/simon_says.rb
+++ week2/homework/simon_says.rb
@@ -1,41 +1,41 @@
 module SimonSays
-	def echo(text)
-		text
-	end
-	def shout(text)
-		text.upcase
-	end
-	def repeat(text,times=2)
-		("#{text} " * times).rstrip
-	end
-	def start_of_word(text,letters=1)
-		text[0,letters]
-	end
-	def first_word(text,words=1)
-		# Could also be done with a regex
-		text.split(" ").first
-	end
+  def echo(text)
+    text
+  end
+  def shout(text)
+    text.upcase
+  end
+  def repeat(text,times=2)
+    ("#{text} " * times).rstrip
+  end
+  def start_of_word(text,letters=1)
+    text[0,letters]
+  end
+  def first_word(text,words=1)
+    # Could also be done with a regex
+    text.split(" ").first
+  end
 end
 
 # UWE-Ruby Answers
 module SimonSays
-	def echo(st)
-		st
-	end
-	
-	def shout(st)
-		st.upcase
-	end
+  def echo(st)
+    st
+  end
 
-	def first_word(st)
-		st.split.first
-	end
+  def shout(st)
+    st.upcase
+  end
 
-	def start_of_word(st,i)
-		st[0...i]
-	end
-	
-	def repeat(st, t=2)
-		([st]*t).join(' ')
-	end
+  def first_word(st)
+    st.split.first
+  end
+
+  def start_of_word(st,i)
+    st[0...i]
+  end
+
+  def repeat(st, t=2)
+    ([st]*t).join(' ')
+  end
 end
