70283905b928a0d9f72a00d255aa04c27164daca
Nikky Southerland <nikky@uw.edu>
2012-10-28 13:28:56 -0700

    Spec

--- week3/homework/calculator.rb
+++ week3/homework/calculator.rb
@@ -1,2 +1,17 @@
 class Calculator
+	def sum(array)
+		return 0 if array.empty?
+		array.inject(:+)
+	end
+	def multiply(array)
+		return 0 if array.empty?
+		array.inject(:*)
+	end
+	def power(n1,n2)
+		n1**n2
+	end
+	def factorial(n)
+		return 0 if n.zero?
+		(1..n).inject(:*)
+	end
 end
