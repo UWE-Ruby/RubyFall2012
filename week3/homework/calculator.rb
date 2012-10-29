00c4fe87f2d0b4edfb917585a993866bc84d2ad6
Steven Curtiss <Steven.Curtiss@infospace.com>
2012-10-28 23:48:27 -0700

    calculator tests and methods

--- week3/homework/calculator.rb
+++ week3/homework/calculator.rb
@@ -1,2 +1,30 @@
 class Calculator
+  def sum(numberArray)
+    if (numberArray.count == 0)
+      return 0
+    end
+    numberArray.inject(:+)
+  end
+
+  def multiply2numbers(firstNumber, secondNumber)
+    firstNumber * secondNumber
+  end
+
+  def multiply(numberArray)
+    if (numberArray.count == 0)
+      return 0
+    end
+    numberArray.inject(:*)
+  end
+
+  def power(baseNumber, exponent)
+    Array.new(exponent, baseNumber).inject(:*)
+  end
+
+  def factorial(number)
+    if (number == 0)
+      return 0
+    end
+    Math.gamma(number + 1)
+  end
 end
