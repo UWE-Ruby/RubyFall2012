dd20058e5fbad017a6ca3c111130e326fa536fe4
Mallory Fontenot <malloryfontenot@gmail.com>
2012-10-29 22:32:51 -0700

    calc file

--- week3/homework/calculator.rb
+++ week3/homework/calculator.rb
@@ -1,2 +1,28 @@
 class Calculator
+  def sum(num_array)
+    num_array.inject(0) do |total, value|
+      total + value
+    end
+  end
+  
+  def mult(x, y)
+    x*y
+  end
+  
+  def multa(num_array)
+    num_array.inject(1) do |total, value|
+      total * value
+    end
+  end
+  
+  def raiseby(x,y)
+    x**y
+  end
+  
+  def factorial(x)
+    (1..x).inject(1) do |total, value|
+      total*value
+    end
+  end
 end
+
