2c78107c534764f0eb2593a3e801b7316e559385
Kat <katf.davis@gmail.com>
2012-10-30 14:27:46 -0700

    week3 homework

--- week3/homework/calculator_spec.rb
+++ week3/homework/calculator_spec.rb
@@ -27,19 +27,64 @@ describe Calculator do
   # Once the above tests pass, 
   # write tests and code for the following:
   
-  it "multiplies two numbers"
+  describe "#multiply" do
+    it "multiplies two numbers with a zero" do
+     @calculator.multiply(1, 0).should == 0
+    end
 
-  it "multiplies an array of numbers"
-  
-  it "raises one number to the power of another number"
-  
+    it "multiplies two numbers with a one" do
+     @calculator.multiply(1, 7).should == 7
+    end
+
+    it "multiplies two numbers with one being itself" do
+     @calculator.multiply(7, 7).should == 49
+    end
+
+    it "multiplies an array of numbers" do
+      @calculator.array_multiply([1,2,3]) == [6]
+    end
+
+    it "multiplies an array of numbers with an empty array" do
+      @calculator.array_multiply([]) == []
+    end
+  end
+
+  describe "#power" do
+    it "raises one number to the power of 0" do
+      @calculator.power(2,0) == 1
+    end
+
+    it "raises one number to the power of 1" do
+      @calculator.power(2,1) == 2
+    end
+
+    it "raises one number to the power of another number" do
+      @calculator.power(2,3) == 8
+    end
+
+  end
+
   # http://en.wikipedia.org/wiki/Factorial
   describe "#factorial" do
-    it "computes the factorial of 0"
-    it "computes the factorial of 1"
-    it "computes the factorial of 2"
-    it "computes the factorial of 5"
-    it "computes the factorial of 10"
+    it "computes the factorial of 0" do
+      @calculator.factorial(0) == 1
+    end
+
+    it "computes the factorial of 1" do
+      @calculator.factorial(1) == 1
+    end
+
+    it "computes the factorial of 2" do
+      @calculator.factorial(2) == 2
+    end
+
+    it "computes the factorial of 5" do
+      @calculator.factorial(5) == 120
+    end
+
+    it "computes the factorial of 10" do
+      @calculator.factorial(10) == 3628800
+    end
   end
 
 end
