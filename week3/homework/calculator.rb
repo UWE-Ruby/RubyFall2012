4d3d684fa9587078e5882b73a86dfafbfb0bb8c3
Peter E. Granger <peter@granger.net>
2012-10-29 22:31:16 -0700

    Committing my changes for week 3 homework

--- week3/homework/calculator.rb
+++ week3/homework/calculator.rb
@@ -1,2 +1,101 @@
 class Calculator
+
+private
+  @@ADD_IDENT = 0 # Addition identity, basis for add operations
+  @@MULT_IDENT = 1 # Multiplicative identity, basis for multiply operations
+
+  # Check to see if a value is an array containing only numbers. For this test,
+  # an empty array returns true, because everything in it is a number (the test
+  # is vacuously true)
+  def Calculator.is_numeric_array?(inArr)
+    # First make sure it's an array
+    result = (inArr.is_a? Array)
+    # Then make sure each of its elements is numeric
+    if result
+      inArr.each { |item| result &= item.is_a? Numeric }
+    end
+    result
+  end
+
+public
+  # Add an array of numbers, returning the sum. If the input is an array which
+  # contains only numbers (including the empty array), it returns the sum of all
+  # numbers in it. For any other input (non-arrays and non-numeric arrays), the
+  # return value is nil.
+  def sum(inArr)
+    result = nil
+    if Calculator.is_numeric_array?(inArr)
+      # Start with the identity
+      result = @@ADD_IDENT
+      # And add each element to the current sum
+      inArr.each { |numVal| result += numVal }
+    end
+    result
+  end
+
+  # Multiply either two numbers, or an array of numbers. Since Ruby doesn't
+  # allow multiple signatures for the same method name, need to handle the
+  # two cases via optional parameters and type checking. (There must be a way
+  # to enforce parameter types, but we haven't seen it yet.) If the arguments
+  # passed are not valid (not two separate numbers, and not a numeric array),
+  # the result is nil. In the case of two numbers, we simply multiply them
+  # together. In the case of a numeric array, all the numbers in it are
+  # multiplied together. The empty array is considered a numeric array, and its
+  # product is the multiplicative identity, 1. This is consistent with the
+  # operations (0! = 1) and (x^0 = 1). It also gives distributive consistency:
+  # Concatenating two arrays together and then taking the product of the result,
+  # yields the same value as taking the products of two arrays, and multiplying
+  # those results together.
+  def product(p1, p2 = nil)
+    result = nil
+    # If we have two parameters, both numeric
+    if ((p1.is_a? Numeric) && (p2.is_a? Numeric))
+      result = p1 * p2
+    # If we just one parameter and it is an array
+    elsif (Calculator.is_numeric_array?(p1) && p2.nil?)
+      # Start with the identity
+      result = @@MULT_IDENT
+      # And multiply each element by the current product
+      p1.each { |numVal| result *= numVal }
+    end
+    result
+  end
+
+  # Raise a number to an integer power. We don't mess around with non-integral
+  # powers. And we certainly don't try to do anything with non-numerics.
+  def exp(base, exp)
+    result = nil
+    negativeExponent = false
+    counter = 0
+    # Make sure base is numeric, exponent is integer
+    if ((base.is_a? Numeric) && (exp.is_a? Integer))
+      negativeExponent = (exp < 0)
+      counter = exp.abs
+      # Start with value of 1, so we're correct if exponent is zero
+      result = @@MULT_IDENT
+      # Repeat multiplying
+      counter.times { result *= base }
+      # If the exponent was negative, take the inverse of the product
+      if (negativeExponent)
+        result = 1.0 / result
+      end
+    end
+    result
+  end
+
+  # Take the factorial value of a non-negative integer. If not an integer, or
+  # if a negative integer, returns nil.
+  def factorial(n)
+    result = nil
+    # Check for valid input
+    if ((n.is_a? Integer) && (n >= 0))
+      # Set base for multiplying
+      result = @@MULT_IDENT
+      # Multiply by values 1 through n
+      n.times do |index|
+        result *= (index + 1) # Index will be 0 through n-1, so increment
+      end
+    end
+    result
+  end
 end
--- week3/homework/calculator_spec.rb
+++ week3/homework/calculator_spec.rb
@@ -8,7 +8,7 @@ describe Calculator do
 
   describe "#sum" do
     it "computes the sum of an empty array" do
-      @calculator.sum([]).should == 0
+      @calculator.sum([]).should be_zero
     end
     
     it "computes the sum of an array of one number" do
@@ -22,24 +22,177 @@ describe Calculator do
     it "computes the sum of an array of many numbers" do
       @calculator.sum([1,3,5,7,9]).should == 25
     end
+
+    it "returns nil when the array is not entirely numeric" do
+      @calculator.sum([1,3,"pancake",7,"cornflake",11]).should be_nil
+    end
+
+    it "returns nil when the array is not at all numeric" do
+      @calculator.sum(["one", :two, "three", :four, "five"]).should be_nil
+    end
+
+    it "returns nil if it's passed a non_array" do
+      @calculator.sum("zombie").should be_nil
+    end
+
+    it "returns nil if it's passed a nil" do
+      @calculator.sum(nil).should be_nil
+    end
   end
   
   # Once the above tests pass, 
   # write tests and code for the following:
   
-  it "multiplies two numbers"
+  describe "#product" do
+    it "multiplies two numbers" do
+      @calculator.product(6, 7).should == 42
+      @calculator.product(-11, 13).should == (-143)
+      @calculator.product(9.3, 7.22).should == 67.146
+    end
 
-  it "multiplies an array of numbers"
+    it "returns nil when both its arguments are nil" do
+      @calculator.product(nil, nil).should be_nil
+    end
+
+    it "returns nil when either of its arguments are non-numeric" do
+      @calculator.product(1, "99").should be_nil
+      @calculator.product(:one, 5.99).should be_nil
+      @calculator.product("two", :seventy_seven).should be_nil
+      @calculator.product(1, nil).should be_nil
+    end
+
+    it "returns nil when its single argument is nil" do
+      @calculator.product(nil).should be_nil
+    end
+
+    it "returns nil when its argument is not an array" do
+      @calculator.product(:not_an_array).should be_nil
+      @calculator.product("also not an array").should be_nil
+    end
+
+    it "returns nil when its argument is a non-numeric array" do
+      @calculator.product(["alpha", "bravo", "charlie"]).should be_nil
+      @calculator.product([1, "two", 3, 4]).should be_nil
+    end
+
+    it "returns 1 when the array of numbers is empty" do
+      @calculator.product([]).should == 1
+    end
+
+    it "returns the single value when the array contains only one number" do
+      @calculator.product([3]).should == 3
+      @calculator.product([2001]).should == 2001
+      @calculator.product([-333]).should == (-333)
+      @calculator.product([13.13]).should == 13.13
+    end
+
+    it "multiplies an array of numbers" do
+      @calculator.product([2, 5]).should == 10
+      @calculator.product([15, -13, 2]).should == (-390)
+      @calculator.product([6, 2.5, -3, 6.1]).should == (-274.5)
+      @calculator.product([-11, 16, -0.25, 3, 1.5]).should == 198
+    end
+
+    it "maintains distributive consistency" do
+      a = [1, 2, 3]
+      b = [-7, -8, -9]
+      c = [3.14, 100.0, -5.55]
+      @calculator.product(a).should == 6
+      @calculator.product(b).should == (-504)
+      @calculator.product(c).should == (-1742.7)
+      # 6 * (-504) = (-3024)
+      @calculator.product(a + b).should == (@calculator.product(a) * @calculator.product(b))
+      # (-504) * (-1742.7) = 878320.8
+      @calculator.product(b + c).should be_within(1.0e-8).of(@calculator.product(b) * @calculator.product(c))
+      # (-1742.7) * 6 = −10456.2 
+      @calculator.product(c + a).should == (@calculator.product(c) * @calculator.product(a))
+      # 6 * (-504) * (-1742.7) = 5269924.8
+      @calculator.product(a + b + c).should == (@calculator.product(a) * @calculator.product(b) * @calculator.product(c))
+
+      # And because the identity is defined as 1, concatenating the empty array is the same as multiplying by 1
+      d = []
+      @calculator.product(a + d).should == (@calculator.product(a) * @calculator.product(d))
+      @calculator.product(d + a + b + c).should == (@calculator.product(d) * @calculator.product(a) * @calculator.product(b) * @calculator.product(c))
+    end
+  end
   
-  it "raises one number to the power of another number"
+  describe "#exp" do
+    it "raises one integer to the power of another integer" do
+      @calculator.exp(2, 8).should == 256
+      @calculator.exp(5, 6).should == 15625
+      @calculator.exp(-3, 7).should == (-2187)
+      @calculator.exp(-11, 4).should == 14641
+      @calculator.exp(-35, 1).should == (-35)
+      @calculator.exp(77, 0).should == 1
+      @calculator.exp(1, 99).should == 1
+    end
+
+    it "raises an integer to a negative integer power" do
+      @calculator.exp(2, -3).should == 0.125
+      @calculator.exp(10, -4).should == 0.0001
+      @calculator.exp(-5, -3).should == (-0.008)
+      @calculator.exp(8, -1).should == 0.125
+      @calculator.exp(1, -33).should == 1
+    end
+
+    it "raises a float to the power of an integer" do
+      @calculator.exp(2.2, 3).should be_within(1.0e-8).of(10.648)
+      @calculator.exp(5.5, 4).should == 915.0625
+      @calculator.exp(-0.25, -2).should == 16
+      @calculator.exp(-6.3, -5).should be_within(1.0e-9).of(-0.000100762)
+      @calculator.exp(3.77, 0).should == 1
+      @calculator.exp(8.225, 1).should == 8.225
+    end
+
+    it "returns nil if we don't have a number raised to an integer" do
+      @calculator.exp(5, 3.5).should be_nil
+      @calculator.exp(9.9, nil).should be_nil
+      @calculator.exp(nil, 5).should be_nil
+      @calculator.exp("fish", "wanda").should be_nil
+      @calculator.exp([:a, :b, :c], 3).should be_nil
+    end
+
+  end
   
   # http://en.wikipedia.org/wiki/Factorial
   describe "#factorial" do
-    it "computes the factorial of 0"
-    it "computes the factorial of 1"
-    it "computes the factorial of 2"
-    it "computes the factorial of 5"
-    it "computes the factorial of 10"
+    it "computes the factorial of 0" do
+      @calculator.factorial(0).should == 1
+    end
+
+    it "computes the factorial of 1" do
+      @calculator.factorial(1).should == 1
+    end
+
+    it "computes the factorial of 0" do
+      @calculator.factorial(2).should == 2
+    end
+
+    it "computes the factorial of 5" do
+      @calculator.factorial(5).should == 120
+    end
+
+    it "computes the factorial of 10" do
+      @calculator.factorial(10).should == 3628800
+    end
+
+    it "does nothing for non-numeric values" do
+      @calculator.factorial(nil).should be_nil
+      @calculator.factorial("word").should be_nil
+      @calculator.factorial([1, 2, 3]).should be_nil
+    end
+
+    it "does nothing for non-integer values" do
+      @calculator.factorial(0.5).should be_nil
+      @calculator.factorial(-3.7).should be_nil
+      @calculator.factorial(9.999).should be_nil
+    end
+
+    it "does nothing for negative integer values" do
+      @calculator.factorial(-1).should be_nil
+      @calculator.factorial(-12).should be_nil
+      @calculator.factorial(-9999).should be_nil
+    end
   end
 
 end
