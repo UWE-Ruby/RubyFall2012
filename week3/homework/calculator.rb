520746368d775a8252ca547e360b0c7a0c318430
gregstallingsuw <gregstallingsuw@gmail.com>
2012-10-30 13:36:16 -0700

    Complete my week3 homework

--- week3/homework/calculator.rb
+++ week3/homework/calculator.rb
@@ -1,2 +1,49 @@
 class Calculator
+
+  # Initializes the calculator.
+  def initialize
+  end
+
+  # Returns the sum of the given numbers or an array of numbers.
+  def sum(*arr)
+    arr.flatten!
+
+    arr.each{ |n| raise ArgumentError, "arguments must be of type Number or an 
+      Array of variables all of type Numeric" unless n.is_a? Numeric }
+
+    if arr.length > 0
+      arr.inject(:+)
+    else
+      0
+    end
+  end
+
+  # Returns the product of the given numbers or an array of numbers.
+  def product(*arr)
+    arr.flatten!
+
+    arr.each{ |n| raise ArgumentError, "arguments must be of type Numeric or an 
+      Array of variables all of type Numeric" unless n.is_a? Numeric }
+
+    arr.inject(:+)
+  end
+
+  # Returns the result of the base number raised to the power of the exponent number.
+  def pow(base, exponent)
+    raise ArgumentError, "base must be of type Numeric" unless base.is_a? Numeric
+    raise ArgumentError, "exponent must be of type Numeric" unless exponent.is_a? Numeric
+
+    base ** exponent
+  end
+
+  # Returns the factorial of the specified number.
+  def factorial(num)
+    raise ArgumentError, "argument must be a non-negative integer" if !(num.is_a? Integer) || num < 0
+
+    if num == 0
+      1
+    else
+      num * factorial(num - 1)
+    end
+  end
 end
--- week3/homework/calculator_spec.rb
+++ week3/homework/calculator_spec.rb
@@ -1,7 +1,7 @@
 require "#{File.dirname(__FILE__)}/calculator"
 
 describe Calculator do
-  
+
   before do
     @calculator = Calculator.new
   end
@@ -10,36 +10,51 @@ describe Calculator do
     it "computes the sum of an empty array" do
       @calculator.sum([]).should == 0
     end
-    
+
     it "computes the sum of an array of one number" do
       @calculator.sum([7]).should == 7
     end
-    
+
     it "computes the sum of an array of two numbers" do
       @calculator.sum([7,11]).should == 18
     end
-    
+
     it "computes the sum of an array of many numbers" do
       @calculator.sum([1,3,5,7,9]).should == 25
     end
   end
-  
-  # Once the above tests pass, 
-  # write tests and code for the following:
-  
-  it "multiplies two numbers"
 
-  it "multiplies an array of numbers"
-  
-  it "raises one number to the power of another number"
-  
-  # http://en.wikipedia.org/wiki/Factorial
-  describe "#factorial" do
-    it "computes the factorial of 0"
-    it "computes the factorial of 1"
-    it "computes the factorial of 2"
-    it "computes the factorial of 5"
-    it "computes the factorial of 10"
+  describe "#product" do
+    it "multiplies two numbers" do
+      @calculator.product(2, 3) == 6
+    end
+
+    it "multiplies an array of numbers" do
+      @calculator.product([2, 3, 4]) == 24
+    end
   end
 
+  describe "#pow" do
+    it "raises one number to the power of another number" do
+      @calculator.pow(2, 4) == 32
+    end
+  end
+
+  describe "#factorial" do
+    it "computes the factorial of 0" do
+      @calculator.factorial(0) == 1
+    end
+    it "computes the factorial of 1" do
+      @calculator.factorial(1) == 1
+    end
+    it "computes the factorial of 2" do
+      @calculator.factorial(2) == 2
+    end
+    it "computes the factorial of 5" do
+      @calculator.factorial(5) == 120
+    end
+    it "computes the factorial of 10" do
+      @calculator.factorial(10) == 3628800
+    end
+  end
 end
