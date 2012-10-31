44b98ca1fed5764d2f121a68eb8ade7fa2921b76
Mick Soum <mick26soum@gmail.com>
2012-10-30 18:01:32 -0700

    week 3 answers

--- week3/homework/calculator.rb
+++ week3/homework/calculator.rb
@@ -1,2 +1,40 @@
 class Calculator
+
+  def sum(array)
+    sum = 0
+    array.each do |value|
+      sum += value
+    end
+    sum
+  end
+
+  def mult(array)
+    array.reject(&:zero?).inject(:*)
+  end
+
+  ### Have Questions on this one
+  def power (base, p)
+  #(1..p).to_a.inject(1){|r,v| r *= base}
+    pow = 1
+    if base == 0
+      pow = 0
+    else
+      p.times do
+        pow *= base
+      end
+      pow
+    end
+  end
+
+  def factorial(range)
+    if range == 0 || range ==1
+      factr =1
+    else
+      factrrange = 1..range
+      factr = factrrange.to_a.inject(:*)
+    end
+    factr
+
+  end
+
 end
--- week3/homework/calculator_spec.rb
+++ week3/homework/calculator_spec.rb
@@ -26,20 +26,39 @@ describe Calculator do
   
   # Once the above tests pass, 
   # write tests and code for the following:
-  
-  it "multiplies two numbers"
 
-  it "multiplies an array of numbers"
   
-  it "raises one number to the power of another number"
+  it "multiplies two numbers"do
+    @calculator.mult([2,2]).should == 4
+  end
+
+  it "multiplies an array of numbers"do
+    @calculator.mult([2,2,2]).should == 8
+  end
+
+  it "raises one number to the power of another number" do
+    #p = 1
+    #32.times{ p *= 2 }
+    @calculator.power(2,8).should == 256
+  end
   
   # http://en.wikipedia.org/wiki/Factorial
   describe "#factorial" do
-    it "computes the factorial of 0"
-    it "computes the factorial of 1"
-    it "computes the factorial of 2"
-    it "computes the factorial of 5"
-    it "computes the factorial of 10"
+    it "computes the factorial of 0"do
+      @calculator.factorial(0).should ==1
+    end
+    it "computes the factorial of 1"do
+    @calculator.factorial(1).should ==1
+    end
+    it "computes the factorial of 2"do
+    @calculator.factorial(2).should == 2
+    end
+    it "computes the factorial of 5"do
+      @calculator.factorial(5).should == 120
+    end
+    it "computes the factorial of 10"do
+      @calculator.factorial(10).should == 3628800
+    end
   end
 
 end
