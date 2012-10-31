04a4f8d6148ee571db5e634a2257233d51dd19bc
Tim Piele <timpiele@gmail.com>
2012-10-30 17:31:53 -0700

    week 3 homework commit

--- week3/homework/calculator.rb
+++ week3/homework/calculator.rb
@@ -1,2 +1,17 @@
 class Calculator
+	def sum(array)
+		return 0 if array.empty?
+		array.inject(:+)
+	end
+	def multiply(array)
+		return 0 if array.empty?
+		array.inject(:*)
+	end
+	def power(one, two)
+		one**two
+	end
+	def factorial(num)
+		return 0 if num.zero?
+		(1..num).inject(:*)
+	end
 end
--- week3/homework/calculator_spec.rb
+++ week3/homework/calculator_spec.rb
@@ -1,4 +1,4 @@
-require "#{File.dirname(__FILE__)}/calculator"
+require "./calculator"
 
 describe Calculator do
   
@@ -6,40 +6,55 @@ describe Calculator do
     @calculator = Calculator.new
   end
 
-  describe "#sum" do
+  describe "sum" do
     it "computes the sum of an empty array" do
       @calculator.sum([]).should == 0
     end
     
     it "computes the sum of an array of one number" do
-      @calculator.sum([7]).should == 7
+      @calculator.sum([17]).should == 17
     end
     
     it "computes the sum of an array of two numbers" do
-      @calculator.sum([7,11]).should == 18
+      @calculator.sum([7,17]).should == 24
     end
     
     it "computes the sum of an array of many numbers" do
-      @calculator.sum([1,3,5,7,9]).should == 25
+      @calculator.sum([1,3,5,7,9, 11, 12]).should == 48
     end
   end
-  
-  # Once the above tests pass, 
-  # write tests and code for the following:
-  
-  it "multiplies two numbers"
 
-  it "multiplies an array of numbers"
-  
-  it "raises one number to the power of another number"
-  
-  # http://en.wikipedia.org/wiki/Factorial
-  describe "#factorial" do
-    it "computes the factorial of 0"
-    it "computes the factorial of 1"
-    it "computes the factorial of 2"
-    it "computes the factorial of 5"
-    it "computes the factorial of 10"
+  describe "multiply" do
+    it "multiplies two numbers" do
+      @calculator.multiply([3,4]).should == 12
+    end
+    it "multiplies an array of numbers" do
+      @calculator.multiply([5,7,9]).should == 315
+    end
+  end
+
+  describe "power" do
+    it "raises number to a power" do
+      @calculator.power(2,3).should == 8
+    end
+  end
+
+  describe "factorial" do
+    it "computes the factorialof 0" do
+      @calculator.factorial(0).should == 0
+    end
+    it "computes the factorial of 0" do
+      @calculator.factorial(1).should == 1
+    end
+    it "computes the factorial of 2" do
+      @calculator.factorial(2).should == 2
+    end
+    it "computes the factorial of 5" do
+      @calculator.factorial(5).should == 120
+    end
+    it "computes the factorial of 10" do
+      @calculator.factorial(10).should == 3628800
+    end
   end
 
 end
