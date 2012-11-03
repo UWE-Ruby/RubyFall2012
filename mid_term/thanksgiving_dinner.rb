833454864bb0a8179615d9d7a1c47b703a98b175
Kris Luminar <kris.luminar@gmail.com>
2012-11-03 14:30:17 -0700

    ThanksgivingDinner can now be initialized with a menu and can add guests.

--- mid_term/thanksgiving_dinner.rb
+++ mid_term/thanksgiving_dinner.rb
@@ -1,5 +1,12 @@
 require_relative 'dinner'
 
 class ThanksgivingDinner < Dinner
-  #
+  attr_accessor :guests
+  attr_reader :menu
+
+  def initialize(menu)
+    @menu = {}
+    @menu[:diet] = menu
+  end
 end
+
