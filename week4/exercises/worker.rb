f3e8e77d29bd805f73b8b6346cea3b074022b9fd
Ben Woodall <mail@benwoodall.com>
2012-11-03 14:41:23 -0700

    updated worker again

--- week4/exercises/worker.rb
+++ week4/exercises/worker.rb
@@ -1,14 +1,7 @@
 class Worker
-<<<<<<< HEAD
 
-	def self.work(num=1, &block)
-		num.times { return block.call }
-	end
-
-end
-=======
 	def self.work(n=1)
 		n.times.inject(nil){yield}
 	end
+
 end
->>>>>>> 9c18a234b31d38e09197996c7784b3944c89148e
