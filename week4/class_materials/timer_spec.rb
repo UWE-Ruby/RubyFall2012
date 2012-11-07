b86f3d2033811224e997b6af3cdfb58fee74aafe
Sol W <capnsol@gmail.com>
2012-11-06 16:04:41 -0800

    commiting week4 homework questions

--- week4/class_materials/timer_spec.rb
+++ week4/class_materials/timer_spec.rb
@@ -19,13 +19,13 @@ describe Timer do
 
 	it "should run our code multiple times" do
 		counter = 0
-		result = Timer.time_code(17) {could += 1}
-		counter.should == 17
+		result = Timer.time_code(17) {counter += 1}
+		counter.should equal 17
 	end
 
 	it "should give the average time" do
 		Time.stub(:now).and_return(0,10)
-		result = Timer.time_code(10) {sleep(1)}
-		result.should be_within(0.1).of(10)
+		result = Timer.time_code(10) { }
+		result.should be_within(0.1).of(1)
 	end
 end
--- week4/exercises/worker.rb
+++ week4/exercises/worker.rb
@@ -1,9 +1,5 @@
 class Worker
-  def self.work(n=1)
-    result = nil
-    n.times do
-      result = yield   
-    end 
-    result
-  end
+	def self.work(n=1)
+		n.times.inject(nil){yield}
+	end
 end
