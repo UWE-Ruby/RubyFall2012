6f6adb1f248eb1063ce68f18dbfddc4ff2c97bd4
Josh Rubinstein <josh@estately.com>
2012-11-07 22:05:06 -0800

    class work and exercises

---
+++ week5/class_materials/skier.rb
@@ -0,0 +1,16 @@
+class Skier
+	#getting events instance variable from initialize 
+	#attr_accessor is shorthand for 
+	# def events @events end and def events=(events) @events = events end
+	attr_accessor :events
+	
+        # don't forget
+	def initialize(stuff=[:gs,:downhill])
+            #instance variables, without instance variable you have no state at all, objects are cooler with states
+	    @events = stuff
+	end
+
+	def ski(event)
+	  "This skier is skiing #{event}"
+	end
+end
