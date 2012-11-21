a130669c8e524705922b4d5528033d8dcda2f718
Kris Luminar <kris.luminar@gmail.com>
2012-11-20 18:10:17 -0800

    merged in Renee's work

--- mid_term/turkey.rb
+++ mid_term/turkey.rb
@@ -1,18 +1,24 @@
-require_relative 'animal'
-
+class Animal
+end
 class Turkey < Animal
-
-  attr_reader :weight
-
-  def initialize(weight = 0)
-    @weight = weight
-  end
-
-  def gobble_speak(str)
-    out_str = ""
-    out_str = str.gsub(/[A-Z]{1}[a-z]*/, "Gobble")      # replace capitalized words with 'Gobble'
-    out_str.gsub!(/\b[a-z]+\b/, "gobble")               # replace lowercase words with 'gobble'
-    out_str.gsub!(/[A-Z]{1}[a-z]+'[a-z]+/, "Gobb'le")   # replace capitalized words with an appostrophe with "Gobb'le"
-    out_str.chomp!('.')                                 # remove trailing period
+	attr_accessor :weight
+	def initialize(weight)
+		@weight = weight
+	end
+	
+	def gobble_speak(monkey_talk)
+	  monkey_talk.gsub(/\b\w*/) do |w|
+	  	if w[0] =~ /[A-Z]/
+	  		"Gobble"
+	  	elsif w[0] =~ /[a-z]/
+	  		"gobble"
+	  	end
+	  end.gsub(/\b\w*'\w*\b/) do |w| 
+	  	if w[0]=='G'
+	  		"Gobb'le" 
+	  	else
+	  		"gobb'le"
+	  	end
+	  end
   end
 end
