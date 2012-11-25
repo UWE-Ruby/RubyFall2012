857cee5ab72a570767209d0e39e692b1ebcf58f5
Eddie Lee <eddielee@email.com>
2012-11-25 01:59:12 -0800

    homework07 files

---
+++ week7/homework/features/step_definitions/pirate-steps.rb
@@ -0,0 +1,19 @@
+Gangway /^I have a PirateTranslator$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Blimey /^I say 'Hello Friend'$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Blimey /^I hit translate$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Letgoandhaul /^it prints out 'Ahoy Matey'$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Letgoandhaul /^it also prints 'Shiber Me Timbers You Scurvey Dogs!!'$/ do
+  pending # express the regexp above with the code you wish you had
+end
