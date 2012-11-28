e13185af8b3ad03722ce8b6dcc70e6b56cdeb672
Ben Woodall <mail@benwoodall.com>
2012-11-27 18:04:13 -0800

    finished homework

--- week7/homework/features/step_definitions/pirate_steps.rb
+++ week7/homework/features/step_definitions/pirate_steps.rb
@@ -2,18 +2,18 @@ Gangway /^I have a PirateTranslator$/ do
   @trans = PirateTranslator.new
 end
 
-Blimey /^I say 'Hello Friend'$/ do
-  @trans.say 'Hello Friend'
+Blimey /^I say '(.*)'$/ do |arg|
+  @trans.say arg
 end
 
 Blimey /^I hit (\w+)$/ do |arg1|
   @piratey = @trans.send(arg1)
 end
 
-Letgoandhaul /^it prints out 'Ahoy Matey'$/ do
-  @piratey.should =~ /Ahoy Matey/
+Letgoandhaul /^it prints out '(.*)'$/ do |arg|
+  @piratey.include? arg
 end
 
-Letgoandhaul /^it also prints 'Shiber Me Timbers You Scurvey Dogs!!'$/ do
-	@piratey.should =~ /Shiber Me Timbers You Scurvey Dogs!!/
+Letgoandhaul /^it also prints '(.*)'$/ do |arg|
+	@piratey.include? arg
 end
