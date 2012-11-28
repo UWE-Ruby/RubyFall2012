70a49ae625f2e37b673dfec9c7efd7ae3dca8c87
root <root@btorretta-VirtualBox.(none)>
2012-11-27 18:07:25 -0800

    questions.txt

---
+++ week7/homework/features/step_definitions/pirate.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+class Pirate
+def initialize(value)
+@value = value
+end
+ 
+def translate(arg1)
+if arg1 = ("Hello Friend") 
+then puts "Ahoy Matey" 
+puts "Shiber Me Timbers You Scurvey Dogs!!"
+end
+end
+end
