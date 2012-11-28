2810fdaf3ad0d37a4b70ff126a24782a54372d05
cherimarie <cherimarie@gmail.com>
2012-11-27 17:17:32 -0800

    a different approach - passes cucumber tests but maybe isn't actually doing what it ought to

--- week7/homework/features/step_definitions/pirate.rb
+++ week7/homework/features/step_definitions/pirate.rb
@@ -3,20 +3,12 @@ class PirateTranslator
 	@translator = PirateTranslator.new()
 
 	def translate(english)
-		#english.gsub(/\b\w*/) do |p|
-		#	if p[0] +~ /[H]/
-		#		put "Ahoy"
-
-		#	elsif p[0] +~ /[F]/
-		#		put "Matey"
-		#	else 
-		#		put "Eeek!"
-		#	end
-		#I keep getting "cannot convert nil to string" errors with this gsub, so commenting it out in favor of code that meets spec but doesn't actually do anything
-			puts "Ahoy Matey"
+		#this isn't the greatest solution, but at least it does something and offers some flexibility.
+		english.sub(/Hello/, 'Ahoy')
+		english.sub(/Friend/, 'Matey')
+		english.sub(/w*\b/, 'Eek!')
 			puts "Shiber Me Timbers You Scurvey Dogs!!"
-	end		
-
+	end	
+		
 #end
-
 end
