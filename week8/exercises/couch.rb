66660e914e8903649c15035f4cd72df8d358fc4d
tallbryan <b_d_williams@yahoo.com>
2012-11-28 22:02:26 -0800

    couch - make a class respond to anything

--- week8/exercises/couch.rb
+++ week8/exercises/couch.rb
@@ -4,17 +4,34 @@ class Couch
 		@cushions = cushions
 	end
 
-	def pillow_colors
-		@pillows.join(", ")
-	end
+#	def pillow_colors
+	#	@pillows.join(", ")
+#	end
 
-	def cushion_colors
-		@cushions.join(", ")
-	end
+	#def cushion_colors
+	#	@cushions.join(", ")
+	#end
 
 	[:pillows, :cushions].each do |s|
 		define_method("how_many_#{s}") do
 			instance_variable_get("@#{s}").count
 		end
 	end
+	
+	[:pillow, :cushion].each do |s|
+	  define_method ("#{s}_colors") do
+	    instance_variable_get("@#{s}s").join(", ")
+    end
+  end
+  
+  def method_missing(meth,*args,&block)
+    puts "You called #{meth} with #{args.join(' ')}"
+    Self.class.class_eval do
+      define_method(meth) do
+        puts "hi"
+      end
+    end
+    
+    super
+  end
 end
