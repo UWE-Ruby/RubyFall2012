55e385093ad01303730fd3a41e2819d75d282656
tallbryan <b_d_williams@yahoo.com>
2012-11-28 23:41:42 -0800

    first 4 passed

---
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -0,0 +1,19 @@
+class TicTacToe
+  attr_reader :player
+  attr_reader :current_player
+  
+  
+  def player=player
+    #set the player
+    @player=player
+    #decide who goes first
+    @current_player = [@player, "Computer"][rand(2)]
+    puts @current_player
+  end
+  
+  def welcome_player
+    #welcome the player
+    "Welcome #{@player}"
+  end
+
+end
