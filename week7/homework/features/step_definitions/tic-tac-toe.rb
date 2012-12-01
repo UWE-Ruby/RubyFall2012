1b28cc0c8ce5d650ea760a176cf42c7d165210c5
Kris Luminar <kris.luminar@gmail.com>
2012-11-30 22:50:41 -0800

    And randomly chooses who goes first

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -1,7 +1,18 @@
 class TicTacToe
   attr_accessor :player
 
+  def initialize
+    @new_game = true
+  end
+
   def welcome_player
     "Welcome #{player}"
+  end
+
+  def current_player
+    if @new_game
+      @new_game = false
+      return [player, "Computer"].sample
+    end
   end
 end
