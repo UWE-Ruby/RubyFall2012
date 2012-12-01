098dcfdacc0f6d9239b1d96be9ac8ad134d21c8c
Kris Luminar <kris.luminar@gmail.com>
2012-11-30 23:48:18 -0800

    And who is X and who is O

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -1,6 +1,9 @@
 class TicTacToe
   attr_accessor :player
 
+  SYMBOLS = [:x, :o]
+  OPPONENTS = [:player, :computer]
+
   def initialize
     @new_game = true
   end
@@ -12,7 +15,21 @@ class TicTacToe
   def current_player
     if @new_game
       @new_game = false
+      assign_x_and_o
       return [player, "Computer"].sample
+    end
+  end
+
+  private
+
+  def assign_x_and_o
+    x_or_o = SYMBOLS.shuffle
+    OPPONENTS.each do |op|
+      self.class.class_eval do
+        define_method("#{op}_symbol") do
+          x_or_o.pop
+        end
+      end
     end
   end
 end
