a4d6c90e8cc1b350c4928799e038bc6d9da783ad
tallbryan <b_d_williams@yahoo.com>
2012-12-01 11:41:25 -0800

    5 tests passing, i think

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -1,15 +1,20 @@
 class TicTacToe
   attr_reader :player
   attr_reader :current_player
-  attr_reader :SYMBOLS
+  attr_reader :player_symbol
+  attr_reader :computer_symbol
   
-  
+  SYMBOLS = ["X","O"]
   def player=player
-    #set the player
-    @player=player
-    #decide who goes first
-    @current_player = [@player, "Computer"][rand(2)]
-    puts @current_player
+    @player=player #set the player name
+    @current_player = [@player, "Computer"][rand(2)] #decide who goes first
+    puts @current_player #show the player name
+    op = {0=>1,1=>0} #define a hash that treats 1 and zero as opposites
+    n = rand(2)
+    @player_symbol = SYMBOLS[n]
+    n = op[n]
+    @computer_symbol = SYMBOLS[n]
+    
   end
   
   def welcome_player
