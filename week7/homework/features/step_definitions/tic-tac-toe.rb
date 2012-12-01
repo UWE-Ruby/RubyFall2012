173ec1fb53c263c382d8dd03681beba5f457b7fd
Kris Luminar <kris.luminar@gmail.com>
2012-12-01 15:32:38 -0800

    move player logic into internal private classes
      - I can't think of a logical reason why TicTacToe needs to know so much about it's players,
        so I put them in their own classes

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -1,25 +1,28 @@
 class TicTacToe
   attr_accessor :player
 
-  SYMBOLS = [:x, :o]
   OPPONENTS = [:player, :computer]
+  SYMBOLS = [:x, :o]
 
-  def initialize(playa = :player)
-    player = playa
+  def initialize(first_player = nil)
     @new_game = true
+    create_opponents(first_player)
     assign_x_and_o
+    pick_first_player
   end
 
   def welcome_player
     "Welcome #{player}"
   end
 
+  def pick_first_player
+    next_up = [player, "Computer"].sample
+  end
+
   def current_player
     if @new_game
       @new_game = false
-      next_up = [player, "Computer"].sample
-      initialize_state_tracker(next_up)
-      return next_up
+      return player
     else
       @player_state_tracker.rotate!.first
     end
@@ -29,24 +32,52 @@ class TicTacToe
     puts "#{player}'s Move:"
   end
 
+  #accessors
+
+  def player_symbol=(value)
+    @human.my_symbol = value
+  end
+
+  def player_symbol
+    @human.my_symbol
+  end
+
+  def computer_symbol=(value)
+    computer.my_symbol = value
+  end
+
+  def computer_symbol
+    @computer.my_symbol
+  end
+
   private
 
-  def initialize_state_tracker(next_up)
-    proc = Proc.new {|human| [human, "Computer"]}
-    @player_state_tracker = proc.call(player)
-
-    # line up @player_state_tracker with next_up so that it correctly tracks state
-    @player_state_tracker.rotate! unless @player_state_tracker.first == next_up
+  def create_opponents(first_player)
+    @human = Human.new(first_player)
+    @computer = Computer.new(first_player)
   end
 
   def assign_x_and_o
     x_or_o = SYMBOLS.shuffle
-    OPPONENTS.each do |op|
-      self.class.class_eval do
-        define_method("#{op}_symbol") do
-          x_or_o.pop
-        end
-      end
+    @human.my_symbol = x_or_o.pop
+    @computer.my_symbol = x_or_o.pop
+  end
+
+  class Player
+    attr_accessor :name, :up_next, :my_symbol
+
+    def initialize(up_next_toggle)
+      up_next = up_next_toggle
+    end
+  end
+
+  class Human < Player
+  end
+
+  class Computer < Player
+    def initialize(up_next_toggle)
+      name = "Computer"
+      super
     end
   end
 end
