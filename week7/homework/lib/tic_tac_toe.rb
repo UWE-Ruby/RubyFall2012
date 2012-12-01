854a7e0731200aaf479bac90c78d99d1fefe8edd
Karen Caplan <keaslk@gmail.com>
2012-11-30 20:16:29 -0800

    first scenario passing tests for tictactoe

--- week7/homework/lib/tic_tac_toe.rb
+++ week7/homework/lib/tic_tac_toe.rb
@@ -1,7 +1,20 @@
 class TicTacToe
+
   attr_accessor :player
+
+  def initialize
+    @players = ["Computer", @player]
+    @player_index = rand(2)
+  end
+
   def welcome_player 
     "Welcome #{player}"
   end
+
+  def current_player
+    @players[@player_index]
+  end
+
+
 
 end
