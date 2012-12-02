60b067ae410fb4a9cf1ca315dcea757819a6541d
tallbryan <b_d_williams@yahoo.com>
2012-12-02 15:07:57 -0800

    good up through computers move

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -3,27 +3,54 @@ class TicTacToe
   attr_reader :current_player
   attr_reader :player_symbol
   attr_reader :computer_symbol
-  
-  def initialize(name="Bob")
-    @player = name
-  end
-  
   SYMBOLS = ["X","O"]
-  def player=player
-    @player=player #set the player name
-    @current_player = [@player, "Computer"][rand(2)] #decide who goes first
-    puts @current_player #show the player name
+  
+  def initialize(first_player = nil, first_players_symbol = nil)
+    #expecting :player or :computer for first_player this is used to force the context for testing
+    @player_hash = {:computer => "Computer"}
+    first_player==nil ? randomize_first_player : @current_player = first_player
     op = {0=>1,1=>0} #define a hash that treats 1 and zero as opposites
     n = rand(2)
     @player_symbol = SYMBOLS[n]
     n = op[n]
     @computer_symbol = SYMBOLS[n]
-    
+    play_game
+  end
+  
+  def player=player
+    @player_hash[:player] = player
+    puts @current_player #show the player name
+  end
+  
+  def player
+    @player_hash[:player]
+  end
+  
+  def current_player
+    @player_hash[@current_player]
+  end
+  
+  def randomize_first_player
+    @current_player = [:player, :computer][rand(2)] #decide who goes first
   end
   
   def welcome_player
     #welcome the player
-    "Welcome #{@player}"
+    #{}"Welcome #{@player}"
+    "Welcome #{@player_hash[:player]}"
   end
 
+  def play_game #structure of the game
+    indicate_player_turn
+    get_player_move
+  end
+  
+  def indicate_player_turn
+    puts "#{@player_hash[@current_player]}'s Move:"
+  end
+  
+  def get_player_move(move = ARGV)
+    move
+  end
 end
+
