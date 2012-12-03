ad719f14212f3f01211803d5caffb14758b57f67
Christopher Ferris <christopher.d.ferris@gmail.com>
2012-12-02 23:35:32 -0800

    Completed some tic-tac-toe stuff

--- week7/homework/features/step_definitions/tic_tac_toe.rb
+++ week7/homework/features/step_definitions/tic_tac_toe.rb
@@ -1,21 +1,29 @@
 class TicTacToe
-	def player=(name)
-		@player = name
-	end
+	attr_accessor :player, :current_player, :player_symbol, :computer_symbol
+	SYMBOLS = [:X, :O]
 
 	def welcome_player
 		return "Welcome #{@player}"
 	end
 
-	def current_player
-		r = Random.rand(2).floor
-		if r >= 1 then return @player
-		else return "Computer" end
+	def initialize(player = "Nobody")
+		@player = player
 	end
 
-	def initialize(name)
-		player=(name)
-		
+	def current_player
+		@players ||= ["Computer", @player]
+		r = Random.new
+		@player_symbol = :O
+		@computer_symbol = :X
+		@players[r.rand(2)]
+	end
+
+	def indicate_player_turn
+		puts "#{@player}'s Move:"
+	end
+
+	def get_player_move
+
 	end
 
 end
