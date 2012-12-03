09fb363aa7140c57ecbe80dc9a3cf84bea6d7090
Nicole Lewis <lewis.nicole@gmail.com>
2012-12-02 18:20:38 -0800

    tictactoe after 1 scenario

--- week7/homework/features/step_definitions/pirate.rb
+++
@@ -1,9 +0,0 @@
-class PirateTranslator 
-	def phrase=(value)
-		@phrase = value
-	end
-	def translate
-		translated = @phrase.gsub(/Hello/, 'Ahoy').gsub(/Friend/, 'Matey')
-		return "#{translated}\nShiber Me Timbers You Scurvey Dogs!!"
-	end
-end
