96aa1e75ee808c00b66e76da6b01b1f6253c040f
Sol W <capnsol@gmail.com>
2012-12-04 00:19:23 -0800

    fixed previous hardcoding; about 20 tests passing, though dubious

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -1,33 +1,52 @@
 class TicTacToe
-  def initialize(*start_player)
-    @start_player = start_player
-  end
-
-  SYMBOLS = ["X", "O"]
+  SYMBOLS = [:X, :O]
   # SYMBOLS = [@player_symbol, @computer_symbol]
 
-  attr_accessor :player, :player_symbol, :computer_symbol, :current_player
+  attr_accessor :player, :player_symbol, :computer_symbol, :current_player, :current_turn
 
-  def player=(player)
-    @player = player
+  def initialize(first_player=nil, symbol=nil)
+    @first_player = first_player
+
+    if first_player
+      @current_turn = @first_player
+    end
+    # if player.nil?
+    #   @current_player = ["Renee", "Computer"].sample   # had @player in array
+    
+    # elsif player == :computer
+    #   @current_player = "Computer"
+    # else #player == :player
+    #   @current_player = "Renee" # had @player
+    # end
   end
+
+  
+
+  # def player=(player)
+  #   @player = player
+  # end
 
   def welcome_player
     "Welcome #{@player}"
   end
 
   def current_player
-    @a_player = [@player, "Computer"]
-    return @current_player = @a_player.sample
+    if @first_player.nil?
+      @current_player = [@player, "Computer"].sample
+    else
+      @current_player = {:computer => "Computer", :player => @player || "Player 1"}[@current_turn]
+    end
+    # @a_player = [@player, "Computer"]
+    #return @current_player #= @a_player.sample
   end
 
-  def symbols
-    @player_symbol = SYMBOLS[rand]
+  def player_symbol
+    @player_symbol = SYMBOLS.sample
 
-    if @player_symbol == "X"
-      @computer_symbol = "O"
+    if @player_symbol == :X
+      @computer_symbol = :O
     else
-      @computer_symbol = "X"
+      @computer_symbol = :X
     end
 
     # @player_symbol = SYMBOLS[0]
