861459ae4c4ecc8e8651ff2f40ccf7698a056fcf
Mallory Fontenot <malloryfontenot@gmail.com>
2012-12-03 21:12:20 -0800

    progress on tictactoe

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -1,34 +1,44 @@
 class TicTacToe
   SYMBOLS = [:X, :O]
-  attr_accessor :player,:p_symbol, :whos_turn, :comp, :comp_symbol, :board
+  attr_accessor :player,:p_symbol, :comp, :comp_symbol,:whos_first,:board, :com_move
   
   def initialize (first_player=nil, first_symb=nil)
     @player = pick_first_player(first_player).to_s.capitalize
     pick_player_symbol(first_symb)
-    @board = {":A1"=>"", ":A2"=>"",":A3"=>"",
-      ":B1"=>"",":B2"=>"",":B3"=>"",
-      ":C1"=>"",":C2"=>"",":C3"=>""}
+    @board = {":A1"=>nil, ":A2"=>nil,":A3"=>nil,
+      ":B1"=>nil,":B2"=>nil,":B3"=>nil,
+      ":C1"=>nil,":C2"=>nil,":C3"=>nil}
   end
   
   def pick_first_player(first_player)
     if first_player.nil?
-      start = Random.new
-      start.rand(0..1)
+      start = rand(0..1)
       if start == 0
-        @whos_turn="Computer"
+        @whos_first="Computer"
       else
-        @whos_turn=@player
+        @whos_first=@player
       end
     else
-      @whos_turn = first_player
+      @whos_first = first_player
     end
   end
   
   def pick_player_symbol(first_symbol)
       if first_symbol.nil?
-        @p_symbol = :X
+        start = rand(0..1)
+         if start == 0
+           @p_symbol=:X
+         else
+           @p_symbol=:O
+         end
       else
         @p_symbol = first_symbol
+      end
+      
+      if @p_symbol ==:X
+        @comp_symbol = :O
+      else
+        @comp_symbol = :X
       end
   end
   
@@ -45,7 +55,7 @@ class TicTacToe
   end
   
   def computer_symbol
-    @p_symbol
+    @comp_symbol
   end
   
   def indicate_palyer_turn
@@ -54,6 +64,21 @@ class TicTacToe
   
   def get_player_move
     "And waits for my input of"
+  end
+  
+  def open_spots
+    @open_spots = @board.find_all{|space,value| value.nil?}
+  end
+  
+  def computer_move
+    random_spot = rand(0..@open_spots.count)
+    spot = @open_spots[random_spot]
+    @board[spot[0]] = @comp_symbol
+    spot
+  end
+  
+  def current_state
+    @board.values.map(&:to_s)
   end
   
 end
