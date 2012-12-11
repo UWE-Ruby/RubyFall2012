d70dce3e6f434108603a182d9a1cd684aad4f675
Ben Woodall <mail@benwoodall.com>
2012-12-10 16:11:53 -0800

    aaaaaaaaaaaaaaargh

--- week7/homework/features/step_definitions/tic-tac-toe-steps.rb
+++ week7/homework/features/step_definitions/tic-tac-toe-steps.rb
@@ -43,7 +43,7 @@ Then /^waits for my input of "(.*?)"$/ do |arg1|
   @game.get_player_move
 end
 
-Given /^it is the computers turn$/ do
+Given /^it is the computer's turn$/ do
   @game = TicTacToe.new(:computer, :O)
   @game.current_player.should eq "Computer"
 end
--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -2,23 +2,49 @@ class TicTacToe
 
 	attr_accessor :player, :player_symbol
 
-	def initialize(player="Renee",symbol=:X)
-		@player ||= ""
-		@player_symbol = symbol
-	end
-
 	SYMBOLS = [:X, :O]
 
+	BOARD = {
+      :A1 => " ", :A2 => " ", :A3 => " ",
+      :B1 => " ", :B2 => " ", :B3 => " ",
+      :C1 => " ", :C2 => " ", :C3 => " "
+      }
+
+	WINNING = {
+    1 => ['A1','A2','A3'],
+    2 => ['B1','B2','B3'],
+    3 => ['C1','C2','C3'],
+    4 => ['A1','B1','C1'],
+    5 => ['A2','B2','C2'],
+    6 => ['A3','B3','C3'],
+    7 => ['A1','B2','C3'],
+    8 => ['C1','B2','A3']
+  }
+
+  def initialize(player="Renee",symbol=:X)
+		@player ||= player
+	end
+
 	def welcome_player
+		current_player
 		"Welcome #{@player}"
 	end
 
 	def current_player
-		[@player, "Computer"].sample
+		@current = [@player, "Computer"].sample
+	end
+
+	def player_symbol
+		return :X if @current == @player
+		return :O if @current == "Computer"
 	end
 
 	def computer_symbol
 		player_symbol == :X ? :O : :X
+	end
+
+	def get_player_move
+		gets.chomp
 	end
 
 	def over?
