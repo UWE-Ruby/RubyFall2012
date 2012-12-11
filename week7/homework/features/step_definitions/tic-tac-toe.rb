1ee966854b0411018bcf4fe7b4ad211efbf5b14d
Ben Woodall <mail@benwoodall.com>
2012-12-10 19:49:00 -0800

    omg cucumber

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -1,46 +1,49 @@
 class TicTacToe
 
-	attr_accessor :player, :player_symbol
+	attr_accessor :player, :player_symbol, :current_player
 
 	SYMBOLS = [:X, :O]
 
-  def initialize(player="Renee",symbol=:X)
-		@player ||= player
+  def initialize(player="",symbol=:X)
+
+		@board = {
+	      :A1 => " ", :A2 => " ", :A3 => " ",
+	      :B1 => " ", :B2 => " ", :B3 => " ",
+	      :C1 => " ", :C2 => " ", :C3 => " "
+	      }
+
+		@winning_moves = {
+	    1 => ['A1','A2','A3'],
+	    2 => ['B1','B2','B3'],
+	    3 => ['C1','C2','C3'],
+	    4 => ['A1','B1','C1'],
+	    5 => ['A2','B2','C2'],
+	    6 => ['A3','B3','C3'],
+	    7 => ['A1','B2','C3'],
+	    8 => ['C1','B2','A3']
+	  }
 	end
 
-	@board = {
-      :A1 => " ", :A2 => " ", :A3 => " ",
-      :B1 => " ", :B2 => " ", :B3 => " ",
-      :C1 => " ", :C2 => " ", :C3 => " "
-      }
-
-	@winning_moves = {
-    1 => ['A1','A2','A3'],
-    2 => ['B1','B2','B3'],
-    3 => ['C1','C2','C3'],
-    4 => ['A1','B1','C1'],
-    5 => ['A2','B2','C2'],
-    6 => ['A3','B3','C3'],
-    7 => ['A1','B2','C3'],
-    8 => ['C1','B2','A3']
-  }
-
 	def welcome_player
-		current_player
 		"Welcome #{@player}"
 	end
 
 	def current_player
-		@current = [@player, "Computer"].sample
+		puts @player
+		@player.to_s.capitalize
+	end
+
+	def randomize
+		[@player, "Computer"].sample
 	end
 
 	def player_symbol
-		return :X if @current == @player
-		return :O if @current == :computer
+		return :X if @current_player == @player
+		return :O if @current_player != @player
 	end
 
 	def computer_symbol
-		player_symbol == :X ? :O : :X
+		@player_symbol == :X ? :O : :X
 	end
 
 	def get_player_move
@@ -48,7 +51,7 @@ class TicTacToe
 	end
 
 	def open_spots
-		@board.reject { |k,v| v != " " }.keys
+		@board.select { |k,v| v == " " }.keys
 	end
 
 	def over?
@@ -64,16 +67,22 @@ class TicTacToe
 	end
 
 	def player_move
+		get_player_move
+		@current_player = "Computer"
 	end
 
 	def computer_move
+		rand = open_spots.sample
+		@board[rand] = computer_symbol.to_s
+		@current_player = @player
+		rand
 	end
 
 	def indicate_player_turn
-
 	end
 
 	def current_state
+		@board.values
 	end
 
 	def determine_winner
