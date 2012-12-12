0bc80a783495e411bcb4bfe80a993f612c965dde
wsugg <sugg.will@gmail.com>
2012-12-11 23:46:14 -0800

    added to hi.rb

--- week10/class_materials/hi.rb
+++ week10/class_materials/hi.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require "sinatra/reloader"
 
 get '/hi' do
 	"<font color='pink'>Hello World!</font>"+
@@ -7,5 +8,11 @@ end
 
 get '/' do 
 	"<font color='green'>Ren&eacute;e</font> is the coolest teacher ever!"+
-	"<br/><a href='/hi'>Click Here</a>"
+	"<br/><a href='/hi'>Click Here</a>"+
+	"<br><a href='/bold-text'>click here for red text</a>"
+end
+
+get '/bold-text' do
+	"<font color='Red'><b>Red Bold Text</b></font>"+
+	"<br/><a href='/'>Go Back</a>"
 end
