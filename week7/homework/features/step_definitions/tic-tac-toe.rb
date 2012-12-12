06a6f7c0be391244a0ce2e3cf9b19ccbb46c4126
Eddie Lee <eddielee@email.com>
2012-12-11 17:11:05 -0800

    week7 questions and tictactoe

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -4,29 +4,42 @@ class TicTacToe
 	SYMBOLS = [:X, :O] 
 
 	def initialize(player=nil, symbol=nil)
-		# if player == :computer
-		# 	@current_player = "Computer"
-		# else
-		# 	@player = player
-		# end
-
-		@current_player = player
-		if @current_player == :computer
-			@computer_symbol = symbol
-			@computer_symbol == :X ? @player_symbol = :O : @player_symbol = :X
-		else
-			@current_player = player
-			@player_symbol == symbol 
-			self.computer_symbol
-		end
+		@board = {
+			:A1 => " ", :A2 => " ", :A3 => " ",
+			:B1 => " ", :B2 => " ", :B3 => " ",
+			:C1 => " ", :C2 => " ", :C3 => " "
+		}
+		# if symbol == nil
+		# 	@player_symbol, @computer_symbol = :X, :O
+		# if player == :computer && symbol == :O
+		# 	@computer_symbol = :O
+		# 	@player_symbol = :X
+		# elsif player == :computer && symbol == :X
+		# 	@computer_symbol = :X
+		# 	@player_symbol = :O
+		# elsif player != :computer && symbol == :O
+		# 	@computer_symbol = :X
+		# 	@player_symbol = :O		
+		# else player != :computer && symbol == :X
+		# 	@computer_symbol = :O
+		# 	@player_symbol = :X	
+		# end		
+		current_player(player)		
 	end
 	def welcome_player
 		"Welcome #@player"
 	end
-	def current_player
-		if @current_player == nil 
-			@current_player = [self.player, "Computer"].sample
-		end
+	def current_player(player=nil)
+		# if @current_player == nil 
+			# @current_player = [@player, "Computer"].sample
+		# end
+		if player == :computer
+			@current_player = "Computer"
+		elsif player == nil
+			@current_player = [@player, "Computer"].sample
+		else	
+			@current_player = player
+		end	
 	end
 	def player_symbol
 		@player_symbol = SYMBOLS.sample
@@ -41,8 +54,10 @@ class TicTacToe
 		@move = gets
 	end
 	def open_spots
+		@board.select {|k,v| v == " "}
 	end
 	def computer_move
+	    open_spots.sample
 	end
 	def current_state
 	end
@@ -51,11 +66,6 @@ class TicTacToe
 	def determine_winner
 	end
 	def board
-		{
-			:A1 => :X, :A2 => :O, :A3 => :X,
-			:B1 => :X, :B2 => :O, :B3 => :X,
-			:C1 => :O, :C2 => :X, :C3 => :O
-		}
 	end
 	def spots_open?
 	end
